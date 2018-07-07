c2b8af15a62b391c56f6921b5e3b11096b310ed0
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-07-07 04:44:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ const selectingDrivers = [
   returnFirstTwoDrivers,
   returnLastTwoDrivers
 ];
+
+const createFareMultiplier = function (integer) {
+  return function (fare) {
+    return fare * integer
+  };
+};
